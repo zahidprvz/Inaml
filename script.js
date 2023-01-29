67702db57fc46b05453a945d664fa5c72722519f
Zahid Pervaiz <20021519-143@uog.edu.pk>
2023-01-29 20:17:41 +0500

    Files added

---
+++ script.js
@@ -0,0 +1,43 @@
+window.onload = function() {
+    // Month Day, Year Hour:Minute:Second, id-of-element-container
+    countUpFromTime("Jan 24, 2023 21:15:00", 'countup1'); // ****** Change this line!
+  };
+  function countUpFromTime(countFrom, id) {
+    countFrom = new Date(countFrom).getTime();
+    var now = new Date(),
+        countFrom = new Date(countFrom),
+        timeDifference = (now - countFrom);
+      
+    var secondsInADay = 60 * 60 * 1000 * 24,
+        secondsInAHour = 60 * 60 * 1000;
+      
+    days = Math.floor(timeDifference / (secondsInADay) * 1);
+    years = Math.floor(days / 365);
+    if (years > 1){ days = days - (years * 365) }
+    hours = Math.floor((timeDifference % (secondsInADay)) / (secondsInAHour) * 1);
+    mins = Math.floor(((timeDifference % (secondsInADay)) % (secondsInAHour)) / (60 * 1000) * 1);
+    secs = Math.floor((((timeDifference % (secondsInADay)) % (secondsInAHour)) % (60 * 1000)) / 1000 * 1);
+  
+    var idEl = document.getElementById(id);
+    idEl.getElementsByClassName('years')[0].innerHTML = years;
+    idEl.getElementsByClassName('days')[0].innerHTML = days;
+    idEl.getElementsByClassName('hours')[0].innerHTML = hours;
+    idEl.getElementsByClassName('minutes')[0].innerHTML = mins;
+    idEl.getElementsByClassName('seconds')[0].innerHTML = secs;
+  
+    clearTimeout(countUpFromTime.interval);
+    countUpFromTime.interval = setTimeout(function(){ countUpFromTime(countFrom, id); }, 1000);
+  }
+
+  function disableRightClick() 
+{ 
+alert("Sorry, right click is not allowed !!"); 
+return false; 
+} 
+
+function injectJS(){    
+  var frame =  $('iframe');
+  var contents =  frame.contents();
+  var body = contents.find('body').attr("oncontextmenu", "return false");
+  var body = contents.find('body').append('<div>New Div</div>');    
+}
